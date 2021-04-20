d3e679e9bf42718fbfc61764d855382243ad03bf
Vladimir Kravtsov <vladimir.kravtsov@aquant.io>
2021-04-20 20:53:29 +0300

    test

--- Jammer-server/main.py
+++ Jammer-server/main.py
@@ -1,2 +1,6 @@
+from src.audio2midi import audio2midi
+
 if __name__ == '__main__':
+    audio2midi.run('/Users/vladikravtsov/Jammer/Jammer-server/src/audio-files/Ensoniq-ZR-76-01-Dope-77.wav',
+                   '/Users/vladikravtsov/Jammer/Jammer-server/src/midi-files/Ensoniq-ZR-76-01-Dope-77.midi')
     print('Kaki Gadol')
